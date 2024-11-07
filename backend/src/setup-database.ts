3c67ae86b2924e1df438fbfa8cb88a39d4b1be55
dennismoha <dennismoha2@gmail.com>
2024-11-07 16:05:24 +0300

    added prisma and prisma configuration added db setup

---
+++ backend/src/setup-database.ts
@@ -0,0 +1,21 @@
+import {PrismaClient}  from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default() =>{
+    async function checkConnection() {
+        try {
+          // Attempt a simple query to check the connection
+          await prisma.$queryRaw`SELECT 1`;
+          console.log('PostgreSQL connection is live');
+        } catch (error) {
+          console.error('Error connecting to PostgreSQL', error);
+        } finally {
+          await prisma.$disconnect(); // Disconnect Prisma Client
+        }
+      }
+      
+      checkConnection();
+      
+};
+
