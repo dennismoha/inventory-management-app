e19fb09a67ecbefea0a411548014d4589facb6ed
dennismoha <dennismoha2@gmail.com>
2024-11-07 21:53:20 +0300

    added decorators for validation, cloudinary configuration and readme.md

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,31 +1,43 @@
 import dotenv from 'dotenv';
 import bunyan from 'bunyan';
-// import cloudinary from 'cloudinary'
+import cloudinary from 'cloudinary'
 
 dotenv.config({});
 
 class Config {
-    public DATABASE_URL: string | undefined;
-    public NODE_ENV: string | undefined;
+  public DATABASE_URL: string | undefined;
+  public NODE_ENV: string | undefined;
+  public CLOUD_NAME: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_API_SECRET: string | undefined;
 
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URL || '';
+    this.NODE_ENV = process.env.NODE_ENV || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME;
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY;
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET;
+  }
 
-    constructor() {
-        this.DATABASE_URL = process.env.DATABASE_URL || '';
-        this.NODE_ENV = process.env.NODE_ENV || '';
+  public createLogger(name: string): bunyan {
+    return bunyan.createLogger({ name, level: 'debug' });
+  }
 
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`${key} configuration is undefined`);
+      }
     }
+  }
 
-    public createLogger(name: string): bunyan {
-        return bunyan.createLogger({ name, level: 'debug' });
-    }
-
-    public validateConfig(): void {
-        for (const [key, value] of Object.entries(this)) {
-            if (value === undefined) {
-                throw new Error(`${key} configuration is undefined`);
-            }
-        }
-    }
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUD_NAME,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET
+    });
+  }
 }
 
 export const config: Config = new Config();
