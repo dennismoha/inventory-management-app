874dbcfa97c558b952609d1d6dbdf908f113efe3
dennismoha <dennismoha2@gmail.com>
2025-01-16 10:30:46 +0300

    ignored the toastify error

--- frontend/src/app/utils/notifications/toastify.tsx
+++ frontend/src/app/utils/notifications/toastify.tsx
@@ -26,6 +26,8 @@ export default function ToastProvider({ children }: ToastProviderProps) {
           contextClass[context?.type || "default"] +
           " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
         }
+
+        //@ts-ignore
         bodyClassName={() => "text-sm font-white font-med block p-3"}
         position="bottom-left"
         autoClose={3000}
